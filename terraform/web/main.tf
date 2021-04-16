02d94437fd900d891c550d86b3e95090b367f296
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-04-16 18:01:07 +0100

    create main.tf for jenkins and web

---
+++ terraform/web/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "name" {
+    description = "Name the instance on deploy"
+}
+
+resource "aws_instance" "mariam_web" {
+    ami = "ami-04b9e92b5572fa0d1"
+    instance_type = "t2.micro"
+    key_name = "mariam"
+
+    tags = {
+        Name = "${var.name}"
+    }
+}
