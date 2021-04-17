4ec2c46227c1a79178100074ee867fa3dae40aee
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-04-17 15:50:18 +0100

    add main.tf for server

---
+++ terraform/server/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "name" {
+    description = "Name the instance on deploy"
+}
+
+resource "aws_instance" "admin_server" {
+    ami = "ami-042e8287309f5df03"
+    instance_type = "t2.micro"
+    key_name = "admin"
+
+    tags = {
+        Name = var.name
+    }
+}
